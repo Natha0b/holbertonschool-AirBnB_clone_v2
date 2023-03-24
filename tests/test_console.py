91447261f7e886577d62cad1d88d081cd80f9ce8
Natha0b <laura.barona04@gmail.com>
2023-03-24 18:56:32 -0500

    add information in the file

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-# -*- coding:utf-8 -*-
-"""Unit test for console.py"""
-from console import HBNBCommand
-
-from unittest import TestCase
+""" Module for testing db storage"""
+import unittest
+from models.base_model import Base
+from models.state import State
+from models.engine.db_storage import DBStorage
+from models import storage
+import os
 import inspect
 import pep8
+import MySQLdb
+from unittest.case import skipIf
 
 
-class TestHBNBCommandDoc(TestCase):
-    "Tests documentation and pep8 for HBNBCommand class."
+class TestDBStorageDoc(unittest.TestCase):
+    "Tests documentation and pep8 for DBStorage class."
 
     @classmethod
     def setUpClass(cls):
-        """Sets the whole functions of the class HBNBCommand to be inspected for
+        """Sets the whole functions of the class DBStorage to be inspected for
         correct documentation."""
-        cls.functions = inspect.getmembers(HBNBCommand,
-                                           inspect.isfunction(HBNBCommand))
+        cls.functions = inspect.getmembers(DBStorage,
+                                           inspect.isfunction(DBStorage))
 
     def test_doc_module(self):
         """Tests for docstring presence in the module and the class."""
-        import console
+        from models.engine import db_storage
 
-        self.assertTrue(len(console.__doc__) > 0)
-        self.assertTrue(len(console.HBNBCommand.__doc__) > 0)
+        self.assertTrue(len(db_storage.__doc__) > 0)
+        self.assertTrue(len(db_storage.DBStorage.__doc__) > 0)
 
     def test_doc_fun(self):
         """Tests for docstring presence in all functions of class."""
@@ -34,9 +38,10 @@ class TestHBNBCommandDoc(TestCase):
         """Tests pep8 style compliance of module and test files."""
         p8 = pep8.StyleGuide(quiet=False)
 
-        res = p8.check_files(['console.py'])
+        res = p8.check_files(['models/engine/db_storage.py'])
         self.assertEqual(res.total_errors, 0,
                          "Found code style errors (and warnings).")
-        res = p8.check_files(['tests/test_console.py'])
+        res = p8.check_files(
+            ['tests/test_models/test_engine/test_db_storage.py'])
         self.assertEqual(res.total_errors, 0,
                          "Found code style errors (and warnings).")
