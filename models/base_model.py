979f197f7aab404a6d442e9fbae6d63ea631a610
Natha0b <laura.barona04@gmail.com>
2023-03-25 12:48:23 -0500

    add information in the files test

--- models/base_model.py
+++ models/base_model.py
@@ -19,24 +19,33 @@ class BaseModel:
                         default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        if args:
-            raise TypeError("BaseModel() takes no positional arguments")
-        form = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
-            if kwargs.get('id') is None:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.datetime.now()
-                self.updated_at = datetime.datetime.now()
-            else:
-                for k, v in kwargs.items():
-                    if k == 'created_at' or k == 'updated_at':
-                        v = datetime.datetime.strptime(v, form)
-                    if k != '__class__':
-                        setattr(self, k, v)
-        else:
+        """Instatntiates a new model"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+
+            if "id" not in kwargs:
+                kwargs["id"] = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                kwargs["created_at"] = datetime.now()
+            else:
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
+            if "updated_at" not in kwargs:
+                kwargs["updated_at"] = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f'
+                )
+
+            if "__class__" in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
