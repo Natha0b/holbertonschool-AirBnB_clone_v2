809556e566b5ac9debf4fe08097b1d443fe3c74f
Natha0b <laura.barona04@gmail.com>
2023-03-25 11:20:47 -0500

    add changes in the files test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,124 +1,67 @@
 #!/usr/bin/python3
-'''
-    Testing the DB_storage module.
-'''
-import time
+''' Unit tests for DB storage '''
+import os
 import unittest
-import sys
-from models.engine.db_storage import DBStorage
-from models import storage
+import models
+from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
 from models.state import State
-from models import storage
-from console import HBNBCommand
-from os import getenv
-from io import StringIO
 
 
-db = getenv("HBNB_TYPE_STORAGE")
-
-
-@unittest.skipIf(db != 'db', "Testing DBstorage only")
-class TestDBStorage(unittest.TestCase):
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                 "Only want to test Database storage")
+class testDBStorage(unittest.TestCase):
     '''
-        Testing the DB_Storage class
+    Testing the DB storage class
     '''
-    @classmethod
-    def setUpClass(cls):
+    def test_existence_user(self):
         '''
-            Initializing classes
+        Testing if User class is being created properly
         '''
-        cls.dbstorage = DBStorage()
-        cls.output = StringIO()
-        sys.stdout = cls.output
+        user = User(email="john@snow.com", password="johnpwd")
+        user.save()
+        if user.id in models.storage.all('User'):
+            self.assertTrue(user.password, "johnpwd")
 
-    @classmethod
-    def tearDownClass(cls):
+    def test_existence_amenity(self):
         '''
-            delete variables
+        Testing if Amenity class is being created properly
         '''
-        del cls.dbstorage
-        del cls.output
+        amenity = Amenity(name="Wifi")
+        amenity.save()
+        if amenity.id in models.storage.all():
+            self.assertTrue(amenity.name, "Wifi")
 
-    def create(self):
+    def test_existence_state(self):
         '''
-            Create HBNBCommand()
+        Testing if State class is being created properly
         '''
-        return HBNBCommand()
+        state = State(name="Alaska")
+        state.save()
+        if state.id in models.storage.all():
+            self.assertTrue(state.name, "Alaska")
 
-    def test_new(self):
+    def test_all_method(self):
         '''
-            Test DB new
+        Testing if all() method returns all instances
         '''
-        new_obj = State(name="California")
-        self.assertEqual(new_obj.name, "California")
+        state = State(name="Cali")
+        state.save()
+        amenity = Amenity(name="Cable")
+        amenity.save()
+        user = User(email="john@snow.com", password="johnpwd")
+        user.save()
+        test_me = str(state.id) + str(amenity.id) + str(user.id)
+        if test_me in models.storage.all():
+            self.assertTrue(state.name, "Cali")
 
-    def test_dbstorage_user_attr(self):
+    def test_delete_method(self):
         '''
-            Testing User attributes
+            Tests the delete method in db_storage
         '''
-        new = User(email="melissa@hbtn.com", password="hello")
-        self.assertTrue(new.email, "melissa@hbtn.com")
-
-    def test_dbstorage_check_method(self):
-        '''
-            Check methods exists
-        '''
-        self.assertTrue(hasattr(self.dbstorage, "all"))
-        self.assertTrue(hasattr(self.dbstorage, "__init__"))
-        self.assertTrue(hasattr(self.dbstorage, "new"))
-        self.assertTrue(hasattr(self.dbstorage, "save"))
-        self.assertTrue(hasattr(self.dbstorage, "delete"))
-        self.assertTrue(hasattr(self.dbstorage, "reload"))
-
-    def test_dbstorage_all(self):
-        '''
-            Testing all function
-        '''
-        storage.reload()
-        result = storage.all("")
-        self.assertIsInstance(result, dict)
-        self.assertEqual(len(result), 0)
-        new = User(email="adriel@hbtn.com", password="abc")
-        console = self.create()
-        console.onecmd("create State name=California")
-        result = storage.all("State")
-        self.assertTrue(len(result) > 0)
-
-    def test_dbstorage_new_save(self):
-        '''
-           Testing save method
-        '''
-        new_state = State(name="NewYork")
-        storage.new(new_state)
-        save_id = new_state.id
-        result = storage.all("State")
-        temp_list = []
-        for k, v in result.items():
-            temp_list.append(k.split('.')[1])
-            obj = v
-        self.assertTrue(save_id in temp_list)
-        self.assertIsInstance(obj, State)
-
-    def test_dbstorage_delete(self):
-        '''
-            Testing delete method
-        '''
-        new_user = User(email="haha@hehe.com", password="abc",
-                        first_name="Adriel", last_name="Tolentino")
-        storage.new(new_user)
-        save_id = new_user.id
-        key = "User.{}".format(save_id)
-        self.assertIsInstance(new_user, User)
-        storage.save()
-        old_result = storage.all("User")
-        del_user_obj = old_result[key]
-        storage.delete(del_user_obj)
-        new_result = storage.all("User")
-        self.assertNotEqual(len(old_result), len(new_result))
-
-    def test_model_storage(self):
-        '''
-            Test to check if storage is an instance for DBStorage
-        '''
-        self.assertTrue(isinstance(storage, DBStorage))
+        state = State(name="Texas")
+        state.save()
+        all_stored = models.storage.all()
+        models.storage.delete(state)
+        self.assertTrue(all_stored["State." + state.id])
