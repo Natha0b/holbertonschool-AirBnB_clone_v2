3782d82d835fe9ddf360349e42fe329eb35bab22
Natha0b <laura.barona04@gmail.com>
2023-03-25 12:45:31 -0500

    add information in the files test

--- models/base_model.py
+++ models/base_model.py
@@ -19,33 +19,24 @@ class BaseModel:
                         default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if args:
+            raise TypeError("BaseModel() takes no positional arguments")
+        form = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            if kwargs.get('id') is None:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.datetime.now()
+                self.updated_at = datetime.datetime.now()
+            else:
+                for k, v in kwargs.items():
+                    if k == 'created_at' or k == 'updated_at':
+                        v = datetime.datetime.strptime(v, form)
+                    if k != '__class__':
+                        setattr(self, k, v)
         else:
-
-            if "id" not in kwargs:
-                kwargs["id"] = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                kwargs["created_at"] = datetime.now()
-            else:
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                )
-            if "updated_at" not in kwargs:
-                kwargs["updated_at"] = datetime.now()
-            else:
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                )
-
-            if "__class__" in kwargs:
-                del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
