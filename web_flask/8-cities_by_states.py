dd7f76e84f58510e5beee2302894c5311cf286b1
Natha0b <laura.barona04@gmail.com>
2023-04-06 12:37:43 -0500

    add changes in the file 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""
-script that starts a Flask web application with an route /hbnb
-"""
+"""First flask module for HBNB"""
+
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
+from os import getenv
 app = Flask(__name__)
+env = getenv('HBNB_TYPE_STORAGE')
 
 
 @app.teardown_appcontext
-def teardown_context(self):
+def teardown_close(self):
+    "Closses sqlalchemy session"
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_cities():
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+def cities_by_state_list():
+    """Returns a HTML with states list"""
+    state_list = storage.all(State).values()
+    return render_template('8-cities_by_states.html', st_list=state_list)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
