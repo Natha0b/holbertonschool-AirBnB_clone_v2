ac2301c35b41adc0aed5a16720d09f35a505acbb
Natha0b <laura.barona04@gmail.com>
2023-04-06 16:55:46 -0500

    add changes in the files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-This python script will start a flask web app
+script that starts a Flask web application with an route
 """
-
-
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 
 
+"""instance creation"""
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def sesclose(self):
-    """close"""
+def teardown_context(self):
+    """
+    Call in this method
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def stateslist():
-    """This function will send all the states"""
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', stateobj=states)
+def states_list():
+    """
+    handles HTTP requests related to the status list.
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
