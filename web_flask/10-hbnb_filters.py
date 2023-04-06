1e8605def30ec9be2a953bb1ad99dba9f5d95d0a
Natha0b <laura.barona04@gmail.com>
2023-04-06 16:25:16 -0500

    add changes in the file 8

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-"""
-script that starts a Flask web application with an route /hbnb
-"""
+"""First flask module for HBNB"""
+
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
-
-
+from os import getenv
 app = Flask(__name__)
-"""instance creation"""
+env = getenv('HBNB_TYPE_STORAGE')
 
 
 @app.teardown_appcontext
-def teardown_context(self):
-    """
-    Call in this method
-    """
+def teardown_close(self):
+    "Closses sqlalchemy session"
     storage.close()
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
-    """
-    It handles the /hbnb_filters
-    path and displays the filters page.
-    """
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    amenities = storage.all(Amenity).values()
-
-    return render_template('10-hbnb_filters.html',
-                           states=states, cities=cities, amenities=amenities)
+    """Returns a HTML with states list"""
+    state_list = storage.all(State).values()
+    amenities_list = storage.all(Amenity).values()
+    return render_template('10-hbnb_filters.html', st_list=state_list, amnt_list=amenities_list)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+"""
+script that starts a Flask web application with an route
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
+"""instance creation"""
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(self):
-    """Closes connection with db"""
+def teardown_context(self):
+    """
+    Call in this method
+    """
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_cities():
-    """Returns a list of state objects"""
-
-    return render_template("8-cities_by_states.html",
-                           states=storage.all(State))
+    """
+    handles HTTP requests related to cities by state.
+    """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
