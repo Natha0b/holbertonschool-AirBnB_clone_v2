dace728cc1e96ef00eaa14c4220658be0664d2cf
Natha0b <laura.barona04@gmail.com>
2023-04-06 13:18:42 -0500

    add changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""This python script will start a flask web app"""
+"""starts a Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def sesclose(self):
-    """close"""
+def close(self):
+    """Closes connection with db"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def display_states_and_cities():
-    """This function will send all the states"""
-    states = storage.all("State")
-    sorted_states = sorted(states.values(), key=lambda x: x.name)
+@app.route("/cities_by_states", strict_slashes=False)
+def states_cities():
+    """Returns a list of state objects"""
 
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    return render_template("8-cities_by_states.html",
+                           states=storage.all(State))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
