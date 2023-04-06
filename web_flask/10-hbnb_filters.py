23524b6e3610f88024e0d3d6321ade0ba8dcbdfa
Natha0b <laura.barona04@gmail.com>
2023-04-06 15:52:18 -0500

    add information in the file 10

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+"""
+script that starts a Flask web application with an route /hbnb
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 
+
 app = Flask(__name__)
+"""instance creation"""
 
 
 @app.teardown_appcontext
-def close(self):
-    """Closes connection with db"""
+def teardown_context(self):
+    """
+    Call in this method
+    """
     storage.close()
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def filters():
-    """Returns the info for the filters"""
-    states = storage.all(State)
-    amenities = storage.all(Amenity)
-    return render_template("10-hbnb_filters.html", **(locals()))
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    It handles the /hbnb_filters
+    path and displays the filters page.
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    amenities = storage.all(Amenity).values()
+
+    return render_template('10-hbnb_filters.html',
+                           states=states, cities=cities, amenities=amenities)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host='0.0.0.0', port=5000)
